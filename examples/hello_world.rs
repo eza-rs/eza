cb871ecea3f791300c0c5adef69ec3150d9541bd
undersquire <55647897+undersquire@users.noreply.github.com>
2022-07-01 00:41:54 -0700

    Setup examples

---
+++ examples/hello_world.rs
@@ -0,0 +1,35 @@
+use eza::{
+    event::{Event, EventResult},
+    widget::{Frame, Label},
+    App, AppError,
+};
+
+struct MyApp {
+    frame: Frame,
+    label: Label,
+}
+
+impl MyApp {
+    fn new() -> Self {
+        Self {
+            frame: Frame::new().unwrap(),
+            label: Label::new("Hello, world!").unwrap(),
+        }
+    }
+}
+
+impl App for MyApp {
+    fn init(&mut self) {
+        self.frame.set_title("MyApp");
+        
+        self.frame.add_widget(&self.label);
+    }
+    
+    fn on_event(&mut self, _: &Event) -> EventResult {
+        EventResult::None
+    }
+}
+
+fn main() -> Result<(), AppError> {
+    MyApp::new().run("me.undersquire.HelloWorld")
+}
